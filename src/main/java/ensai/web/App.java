8f45556701fe93a5ebceff93839a1d7a6ba84f0d
Théo Lavolé <theo.lavole@eleve.ensai.fr>
2017-03-09 11:59:51 +0100

    Projet Web 4

--- src/main/java/ensai/web/App.java
+++ src/main/java/ensai/web/App.java
@@ -6,10 +6,16 @@ package ensai.web;
  */
 public class App {
 	public static void main(String[] args) throws Exception {
+		// Obtenir la météo
+		 SearchMeteo meteo = new SearchMeteo();
+		 meteo.getConditions("Rennes");
+		
+		 // Obtenir le latitude et longitude d'une ville (API GoogleMap)
+		new SearchGoogleMap().getLocation("Rennes");
+		
+		// utiliser la loc trouvée avant pour les tweets
 		// near:\"Paris\" within:15mi
 		// :) pour les tweets positifs, :( pour les tweets négatifs
 		new SearchTwitter("#FCBPSG :)");
-		// SearchMeteo meteo = new SearchMeteo();
-		// meteo.getConditions("Rennes");
 	}
 }
---
+++ src/main/java/ensai/web/SearchGoogleMap.java
@@ -0,0 +1,42 @@
+package ensai.web;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+
+import org.json.JSONObject;
+
+import twitter4j.JSONArray;
+
+public class SearchGoogleMap {
+	
+	static void getLocation(String city) throws Exception, IOException {
+	// récupérer le contenu de la page web
+			String url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + city;
+			HttpURLConnection conn = (HttpURLConnection) new URL(url).openConnection();
+			conn.connect();
+			BufferedInputStream bis = new BufferedInputStream(conn.getInputStream());
+			byte[] bytes = new byte[1024];
+			int tmp;
+			String json = "";
+			while ((tmp = bis.read(bytes)) != -1) {
+				String chaine = new String(bytes, 0, tmp);
+				json += chaine;
+			}
+			conn.disconnect();
+			
+			// parser le json
+			JSONObject obj = new JSONObject(json);
+			org.json.JSONObject results = obj.getJSONArray("results").getJSONObject(0);
+			org.json.JSONObject geometry = results.getJSONObject("geometry");
+			org.json.JSONObject location = geometry.getJSONObject("location");		
+			Double latitude = location.getDouble("lat");
+			Double longitude = location.getDouble("lng");
+
+			System.out.println("Latitude : " + latitude);
+			System.out.println("Longitude : " + longitude);
+	}
+	
+}
