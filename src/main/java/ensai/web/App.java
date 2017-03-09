2d761af98ea1b8b306047abc35501e64b3c7ff41
Théo Lavolé <theo.lavole@eleve.ensai.fr>
2017-03-09 11:16:05 +0100

    Projet Web 3

--- src/main/java/ensai/web/App.java
+++ src/main/java/ensai/web/App.java
@@ -1,28 +1,15 @@
 package ensai.web;
 
-import java.io.IOException;
-import java.util.List;
-
-import twitter4j.Paging;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.ResponseList;
-import twitter4j.Status;
-import twitter4j.Twitter;
-import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.conf.ConfigurationBuilder;
-
 /**
  * Hello world!
  *
  */
 public class App {
 	public static void main(String[] args) throws Exception {
-		new SearchTwitter("#FCBPSG");
-//		SearchMeteo meteo = new SearchMeteo();
-//		meteo.getConditions("Rennes");
+		// near:\"Paris\" within:15mi
+		// :) pour les tweets positifs, :( pour les tweets négatifs
+		new SearchTwitter("#FCBPSG :)");
+		// SearchMeteo meteo = new SearchMeteo();
+		// meteo.getConditions("Rennes");
 	}
 }
-
-
--- src/main/java/ensai/web/SearchTwitter.java
+++ src/main/java/ensai/web/SearchTwitter.java
@@ -2,6 +2,7 @@ package ensai.web;
 
 import java.util.List;
 
+import twitter4j.GeoLocation;
 import twitter4j.Query;
 import twitter4j.Status;
 import twitter4j.Twitter;
@@ -10,33 +11,45 @@ import twitter4j.TwitterFactory;
 import twitter4j.conf.ConfigurationBuilder;
 
 public class SearchTwitter {
-	
-	public SearchTwitter (String recherche) throws TwitterException {
-		ConfigurationBuilder cb = new ConfigurationBuilder();
-		cb.setDebugEnabled(true).setOAuthConsumerKey("ePCYTxOV0J28ol4iE2mSxUYej")
-				.setOAuthConsumerSecret("sFsaQzueveR8boFFbbngjQ8DUrQRvNAvGnKGcnzOBo407nz0h8")
-				.setOAuthAccessToken("837234720660406272-g2cbn4CbndBlHS8mXKl5529w0J2sHZC")
-				.setOAuthAccessTokenSecret("Oc1Wl3RLINHYFzEFOdi1yxaMzzIhJvoE2tcPdrzugKzsY");
-		TwitterFactory tf = new TwitterFactory(cb.build());
-		Twitter twitter = tf.getInstance();
-		
-		// Ce qu'on recherche
-		Query q = new Query(recherche);
-		q.setCount(100);
-		List<Status> statuses = twitter.search(q).getTweets();
-		for (Status status : statuses) {
-			// Date
-			System.out.println("Creat : "+status.getCreatedAt());
-			// Nom
-			System.out.println("By : "+status.getUser().getName());
-			// Localisation
-			System.out.println("Loc : "+status.getUser().getLocation());
-			// Message
-			System.out.println("Message : "+ status.getText());
-			// Source
-			System.out.println("Source : "+status.getSource());
-			System.out.println("---------------------------------------------------------");
+
+	public SearchTwitter(String recherche) throws TwitterException {
+		try {
+			ConfigurationBuilder cb = new ConfigurationBuilder();
+			cb.setDebugEnabled(true).setOAuthConsumerKey("ePCYTxOV0J28ol4iE2mSxUYej")
+					.setOAuthConsumerSecret("sFsaQzueveR8boFFbbngjQ8DUrQRvNAvGnKGcnzOBo407nz0h8")
+					.setOAuthAccessToken("837234720660406272-g2cbn4CbndBlHS8mXKl5529w0J2sHZC")
+					.setOAuthAccessTokenSecret("Oc1Wl3RLINHYFzEFOdi1yxaMzzIhJvoE2tcPdrzugKzsY");
+			TwitterFactory tf = new TwitterFactory(cb.build());
+			Twitter twitter = tf.getInstance();
+
+			System.out.println(recherche);
+			// Ce qu'on recherche
+			Query q = new Query(recherche);
+			// Ajouter un emplacement
+			q.geoCode(new GeoLocation(48.856614,2.3522219000000177), 10.00, "km");
+			// dire combien on veut de retours
+			q.setCount(100);
+			
+			List<Status> tweets = twitter.search(q).getTweets();
+			for (Status currentTweet : tweets) {
+				GeoLocation loc = currentTweet.getGeoLocation();
+				if (loc != null) {
+					System.out.println(loc);
+				}
+				// Date
+				System.out.println("Creat : " + currentTweet.getCreatedAt());
+				// Nom
+				System.out.println("By : " + currentTweet.getUser().getName());
+				// Localisation
+				System.out.println("Loc : " + currentTweet.getUser().getLocation());
+				// Message
+				System.out.println("Message : " + currentTweet.getText());
+				// Source
+				System.out.println("Source : " + currentTweet.getSource());
+				System.out.println("---------------------------------------------------------");
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 	}
-	
 }
